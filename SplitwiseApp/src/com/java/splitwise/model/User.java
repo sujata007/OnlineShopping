69480987ae716cb694fca24580dd076902125949
Sujata Boruah <priyadarshinee11@gmail.com>
2022-12-14 00:51:17 +0530

    Adding user class

--- SplitwiseApp/src/com/java/splitwise/model/User.java
+++ SplitwiseApp/src/com/java/splitwise/model/User.java
@@ -1,5 +1,47 @@
 package com.java.splitwise.model;
 
 public class User {
+	private int userId;
+	private String userName;
+	private String email;
+	private String phoneNumber;
+	
+	public User(int userId,String userName,String email,String phoneNumber) {
+		this.userId = userId;
+		this.userName = userName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
 
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
