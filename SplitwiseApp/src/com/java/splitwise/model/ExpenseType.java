0fd0ae3f06ca84e43e3c98ec92b46b062543ac64
sujata007 <priyadarshinee11@gmail.com>
2022-12-15 02:33:22 +0530

    Delete SplitwiseApp directory

--- SplitwiseApp/src/com/java/splitwise/model/ExpenseType.java
+++
@@ -1,5 +0,0 @@
-package src.com.java.splitwise.model;
-
-public enum ExpenseType {
-	EXACT, PERCENTAGE, EQUAL;
-}
--- SplitwiseApp/src/com/java/splitwise/service/UserShareService.java
+++
@@ -1,7 +0,0 @@
-package src.com.java.splitwise.service;
-
-public interface UserShareService {
-	public void showShareOfUser(String username);
-
-	public void showShareOfAllUser();
-}
